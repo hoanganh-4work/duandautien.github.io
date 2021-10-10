163f1e91009eff9496cdbf6c23ad276b18dac87f
HoangAnh <anh.nguyenphuonghoang@gmail.com>
2021-10-10 15:28:30 +0700

    first commit

---
+++ assets/js/main.js
@@ -0,0 +1,7 @@
+const $ = document.querySelector.bind(document)
+const $$ = document.querySelectorAll.bind(document)
+
+const tabs = $$('about-student__container')
+const dots = $$('about-student__owl-dot1')
+
+console.log(tabs, dots)
